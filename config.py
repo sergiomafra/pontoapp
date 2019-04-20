b2eb1e2127e6876d8b323cc1e6f06ec4e7c4c45a
Sérgio Mafra <sergio@mafra.io>
2019-04-20 04:09:13 -0300

    Added support for environment variables

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
+from dotenv import load_dotenv
+
 import os
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+DOTENV_PATH = os.path.join(BASEDIR, '.env')
+load_dotenv(DOTENV_PATH)
 
 class Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ['PONTOAPP_SECRET_KEY']
+    SECRET_KEY = os.environ['PONTOAPP_SECRET_KEY'].rstrip('\n')
+    SQLALCHEMY_DATABASE_URI = os.getenv('PONTOAPP_DATABASE_URL').rstrip('\n')
 
 class ProductionConfig(Config):
     DEBUG = False
